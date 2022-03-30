c14570cbe7dda499194f530c9a25c672ff7fe569
yangfei4913438 <yangfei86@vip.qq.com>
2022-03-31 02:12:35 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
+
+export default App;

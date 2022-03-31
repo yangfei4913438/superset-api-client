f55818b219533ab7f6b735ae0f1099ce7ac2b335
yangfei4913438 <yangfei86@vip.qq.com>
2022-03-31 22:56:39 +0800

    查询某个列的可选值

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  // 权限
   const [token, setToken] = useState('');
+  // 数据
   const [response, setResponse] = useState({});
+  // 查询SQL
   const [sql, setSql] = useState('');
+  // 数据集信息
   const [dataset, setDataset] = useState({ metrics: [], columns: [] });
+  // 维度的可选值
+  const [options, setOptions] = useState([]);
 
   const data = {
     datasource: { id: 145, type: 'table' }, // 确定好数据集的ID，类型不改！
@@ -137,6 +143,47 @@ function App() {
       });
   };
 
+  const getOptions = () => {
+    axios
+      .post(
+        '/api/v1/chart/data',
+        {
+          ...data,
+          queries: [
+            {
+              columns: ['comm_name'],
+              metrics: [
+                {
+                  expressionType: 'SQL',
+                  sqlExpression: 'count(1)',
+                  column: null,
+                  aggregate: null,
+                  isNew: false,
+                  hasCustomLabel: false,
+                  label: 'count(1)',
+                  optionName: 'metric_7n5f67wsfl9_g2x38svidzc',
+                },
+              ], // 这个不用管，用于给分组，名称去重的，否则要自己写去重。
+            },
+          ],
+          result_type: 'full',
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then(res => {
+        console.log('查询项目的可选项:', res);
+        setOptions(res.data.result[0].data);
+      })
+      .catch(err => {
+        console.error('查询项目的可选项出错:', err);
+      });
+  };
+
   return (
     <div className="p-8 space-y-8">
       <article className="flex space-x-8">
@@ -155,6 +202,10 @@ function App() {
         <button className="btn" onClick={datasetInfo}>
           查询数据集信息
         </button>
+
+        <button className="btn" onClick={getOptions}>
+          查询项目名称的可选项
+        </button>
       </article>
 
       <div>
@@ -171,12 +222,17 @@ function App() {
         <h3>数据集信息:</h3>
         <div>
           <h4>列名:</h4>
-          <code>{JSON.stringify(dataset.columns.map(o => o.column_name))}</code>
+          <code>{JSON.stringify(dataset.columns?.map(o => o.column_name))}</code>
         </div>
         <div>
           <h4>指标名称:</h4>
-          <code>{JSON.stringify(dataset.metrics.map(o => o.metric_name))}</code>
+          <code>{JSON.stringify(dataset.metrics?.map(o => o.metric_name))}</code>
         </div>
+      </div>
+
+      <div>
+        <h3>comm_name选项列表：</h3>
+        <code>{JSON.stringify(options?.map(o => o.comm_name))}</code>
       </div>
     </div>
   );
